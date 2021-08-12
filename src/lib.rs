005f8a2970978f43f7a12582c56bad58e81cca74
shinebalance <manazasi2020@outlook.com>
2021-08-12 23:22:15 +0900

    JS内でリストを受け取れるようにする

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,11 @@
+extern crate serde_json;
 use lindera::tokenizer::Tokenizer;
 use lindera_core::core::viterbi::Mode;
-
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
-}
 
 #[wasm_bindgen]
-pub fn greet(text: &str) {
+pub fn greet(text: &str) -> JsValue {
 
     // create tokenizer
     let mut tokenizer = Tokenizer::new(Mode::Normal, "");
@@ -17,24 +13,7 @@ pub fn greet(text: &str) {
     // tokenize the text
     let tokens = tokenizer.tokenize(&text);
 
-    // output the tokens
-    for token in tokens {
-        alert(&format!("-->{}", token.text));
-    }
+    // JSに対応した戻り値を返すので
+    JsValue::from_serde(&tokens).unwrap()
 
 }
-
-// fn main() -> std::io::Result<()> {
-//     // create tokenizer
-//     let mut tokenizer = Tokenizer::new(Mode::Normal, "");
-
-//     // tokenize the text
-//     let tokens = tokenizer.tokenize("東京都の新型コロナウイルスのモニタリング会議が１２日あり、都内の感染状況について専門家から「制御不能な状況だ。災害レベルで感染が猛威を振るう非常事態」と報告された。");
-
-//     // output the tokens
-//     for token in tokens {
-//         println!("{}", token.text);
-//     }
-
-//     Ok(())
-// }
